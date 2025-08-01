42953c5f4a0a119f792f9d64bec95f810db7a19e
Danilo Almeida <cs.daniloalmeida@gmail.com>
2025-07-31 22:37:18 -0300

    feat: redirect to post on creation

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -27,7 +27,7 @@ export class PostController {
     @Res() res: Response,
   ): Promise<void> {
     const id = await this.postService.create(createPostDto);
-    res.status(HttpStatus.CREATED).location(`/posts/${id}`).send();
+    res.redirect(`/posts/${id}`);
   }
 
   @Get(':year/:month/:day/:slug')
